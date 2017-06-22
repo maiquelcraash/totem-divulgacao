02f66e3090dcc3fd6a7e967aaa3ab89179962643
maiquel <maiquel_craash@hotmail.com>
2017-06-21 22:45:42 -0300

    Adicionadas configuracões para rodar em Producão (Jelastic).

--- server/properties.js
+++ server/properties.js
@@ -9,10 +9,15 @@
 	exports.SERVER_PORT = 8082;
 
 	/* DataBase Settings (PostGres Only) */
-	exports.DB_HOST = 'localhost';
+	// exports.DB_HOST = 'localhost';
+	// exports.DB_PORT = 5432;
+	// exports.DB_DATABASE = 'totem';
+	// exports.DB_USER = 'maiquel';
+	// exports.DB_PASSWORD = 'craash1';
+	exports.DB_HOST = 'postgres133264-totem.jelasticlw.com.br';
 	exports.DB_PORT = 5432;
 	exports.DB_DATABASE = 'totem';
-	exports.DB_USER = 'maiquel';
-	exports.DB_PASSWORD = 'craash1';
+	exports.DB_USER = 'webadmin';
+	exports.DB_PASSWORD = 'AAAmrv45149';
 
 }());
