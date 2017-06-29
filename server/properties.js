4f7a70aa484120188ee13d3608146995d22e939d
maiquel <maiquel_craash@hotmail.com>
2017-06-29 17:53:03 -0300

    Adicionado mecanismo para diferenciar ambiente de teste e de producao com variável de ambiente.

--- server/properties.js
+++ server/properties.js
@@ -8,16 +8,19 @@
 	/* Server Settings */
 	exports.SERVER_PORT = 8082;
 
-	/* DataBase Settings (PostGres Only) */
-	// exports.DB_HOST = 'localhost';
-	// exports.DB_PORT = 5432;
-	// exports.DB_DATABASE = 'totem';
-	// exports.DB_USER = 'maiquel';
-	// exports.DB_PASSWORD = 'craash1';
-	exports.DB_HOST = 'postgres133264-totem.jelasticlw.com.br';
-	exports.DB_PORT = 5432;
-	exports.DB_DATABASE = 'totem';
-	exports.DB_USER = 'webadmin';
-	exports.DB_PASSWORD = 'AAAmrv45149';
 
+	if (process.env.NODE_ENV === 'test') {
+		exports.DB_HOST = 'localhost';
+		exports.DB_PORT = 5432;
+		exports.DB_DATABASE = 'totem';
+		exports.DB_USER = 'maiquel';
+		exports.DB_PASSWORD = 'craash1';
+	}
+	else {
+		exports.DB_HOST = 'postgres133264-totem.jelasticlw.com.br';
+		exports.DB_PORT = 5432;
+		exports.DB_DATABASE = 'totem';
+		exports.DB_USER = 'webadmin';
+		exports.DB_PASSWORD = 'AAAmrv45149';
+	}
 }());
